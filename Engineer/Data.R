2f7a407159b50155f5d2b085c33f55134f1b7b6d
tarihart20 <tari.hart@yotes.collegeofidaho.edu>
2026-01-21 14:51:37 -0700

    data for brainstorm

--- Engineer/Data.R
+++ Engineer/Data.R
@@ -1,6 +1,24 @@
 library(tidyverse)
 
-enrollment <- read_csv('data/raw/registrar_data.csv')
+registrar_data <- read_csv('data/raw/registrar_data.csv')
 
-#Tari did this
+clean_data <- registrar_data |>
+  separate(stc_depts, into = c('first_major', 'second_major'), sep=',')
+
+glimpse(clean_data)
+
+clean_data |>
+  select(students_xstu_grad_app_major) |>
+  print(n = 50)
+
+glimpse(clean_data19)
+#twenty_enrollment <- registrar_data |>
+#  filter(term_reporting_year == "2020")
+
+#twentyone_enrollment <- registrar_data |>
+#  filter(term_reporting_year == "2021")
+
+#twentytwo_enrollment <- registrar_data |>
+#  filter(term_reporting_year == "2022")
+
 
