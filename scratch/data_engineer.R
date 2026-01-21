fdd0c858270c90381e992c708f16c23a795e53c4
fynngeh <fynngeh@gmail.com>
2026-01-21 14:43:25 -0700

    creat a clean data set where the department comes from the course name

--- scratch/data_engineer.R
+++ scratch/data_engineer.R
@@ -4,17 +4,20 @@ data_raw <- read.csv("data/raw/registrar_data.csv")
 
 
 clean_enrollments <- data_raw |>
-  select(stc_person, stc_depts, term_numeric, stc_sec_name) |>
+  select(stc_person, stc_course_name, term_numeric, stc_sec_name) |>
   rename(
     student_id = stc_person,
-    department = stc_depts,
     term       = term_numeric,
     course_id  = stc_sec_name
   ) |>
-  mutate(department = str_to_upper(str_squish(department))) |>
-  drop_na() |>
+  mutate(
+    department = str_extract(
+      str_to_upper(str_squish(stc_course_name)),
+      "^[A-Z]+"
+    )
+  ) |>
+  drop_na(student_id, department, term, course_id) |>
   distinct()
-
 
 
 
