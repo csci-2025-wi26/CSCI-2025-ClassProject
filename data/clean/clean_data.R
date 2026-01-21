3a4eb3693e8ac9ed1c222d06cfdaa9763aace00f
Asher Bruce <bruce.asher.j@gmail.com>
2026-01-21 15:03:31 -0700

    begin grad column in clean_data.R, commented out, not done

--- data/clean/clean_data.R
+++ data/clean/clean_data.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(janitor)
 
-raw_data <- read.csv("CSCI-2025-ClassProject/data/raw/registrar_data.csv") |>
+raw_data <- read.csv("data/raw/registrar_data.csv") |>
   clean_names()
 
 glimpse(raw_data)
@@ -31,6 +31,19 @@ cleaned_data <- raw_data |>
     term_index = term_year + term_val
   ) |>
   mutate(primary_major = coalesce(students_stu_active_majors, stu_acad_programs))
+
+# View(cleaned_data)
+# # add a refined grad column
+# grad_column <- cleaned_data |> 
+#   mutate(
+#     grad_year = { # need to handle double values; separate graduate level graduation date column?
+#       if_else(
+#         str_extract(person_xper_grad_term, "(FA|SP|WI|SU)") == "FA", 
+#         2000 + parse_number(person_xper_grad_term), # fall term
+#         2000 + parse_number(person_xper_grad_term) - 1 # spring, winter, and summer terms list academic year
+#       )
+#     }
+#   )
 
 write_csv(cleaned_data, "CSCI-2025-ClassProject/data/clean/registrar_cleaned.csv")
 
