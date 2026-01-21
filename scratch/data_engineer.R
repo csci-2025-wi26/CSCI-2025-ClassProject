548912678948f2c8f0e840e8c1ae30a95fa6a4a6
fynngeh <fynngeh@gmail.com>
2026-01-21 14:15:34 -0700

    made data set clean enrollments

--- scratch/data_engineer.R
+++ scratch/data_engineer.R
@@ -1,6 +1,20 @@
 library(tidyverse)
 
-
 data_raw <- read.csv("data/raw/registrar_data.csv")
-glimpse(data_raw)
+
+
+clean_enrollments <- data_raw |>
+  select(stc_person, stc_depts, term_numeric, stc_sec_name) |>
+  rename(
+    student_id = stc_person,
+    department = stc_depts,
+    term       = term_numeric,
+    course_id  = stc_sec_name
+  ) |>
+  mutate(department = str_to_upper(str_squish(department))) |>
+  drop_na() |>
+  distinct()
+
+
+
 
