90f29c7a7cf40b0b25595d81d16a0067e449c90f
Sawyer-Ellsworth <saw.ellsworth@gmail.com>
2026-01-22 14:02:15 -0700

    started UI on shiny app

--- scratch/out_app.R
+++ scratch/out_app.R
@@ -1,9 +1,18 @@
 library(shiny)
 library(bslib)
+library(tidyverse)
 
-source("scratch/out_global.R")
+source("out_global.R")
 
-ui <- page_fluid()
+
+ui <- page_fluid(
+  selectInput(
+    inputId = "major_select",
+    label = "Select Primary Major:",
+    choices = sort(unique(cleaned_data$primary_major)),
+    selected = "POE"
+  ),
+)
 
 server <- function(input, output, session) {}
 
