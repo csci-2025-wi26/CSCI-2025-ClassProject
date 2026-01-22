f53a857bcc7c5523d5ff7085ac8b96349debe4a6
tarihart20 <tari.hart@yotes.collegeofidaho.edu>
2026-01-22 13:44:03 -0700

    separated races

--- Engineer/enrollment_global.r
+++ Engineer/enrollment_global.r
@@ -5,6 +5,7 @@ enrollment_registrar_data <- read_csv('data/raw/registrar_data.csv')
 
 enrollment_clean_data <- enrollment_registrar_data |>
   separate_longer_delim(students_stu_majors , delim = ',') |>
+  separate_longer_delim(person_per_races, delim = ',') |>
   mutate(students_stu_majors  = trimws(students_stu_majors )) |>
   group_by(stc_person, term_reporting_year) |>
   mutate(major = first(students_stu_majors))
