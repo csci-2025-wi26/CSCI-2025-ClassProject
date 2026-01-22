4699f970e0f82b6ac8bee342e72eaf7bc6dc8f90
jackptheiss <jack.theiss@yotes.collegeofidaho.edu>
2026-01-22 14:58:21 -0700

    small change: changed new dataframe names in extractingsemesterData.R

--- scratch/extractingSemesterData.R
+++ scratch/extractingSemesterData.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(arrow)
 cleaned_data <- read_csv("data/processed/cleaned_data.csv")
 
-cleaned_data <- cleaned_data |> 
+extracting_sem_data <- cleaned_data |> 
   separate(student_course_sec_sec_term, into = c("year_digit", "term"), sep = "/") |> 
   mutate(
     year = 2000 + as.integer(year_digit),
@@ -15,7 +15,7 @@ cleaned_data <- cleaned_data |>
     semester_index = year * 10 + term_order    #This line is creating a numeric column to sort by
   ) 
 
-first_semester_grades_summary <- cleaned_data |> 
+first_semester_grades_summary <- extracting_sem_data |> 
   group_by(stc_person) |> 
   filter(semester_index == min(semester_index, na.rm = TRUE)) |> #select the first semester; min of semester_index
   summarise(
@@ -23,7 +23,7 @@ first_semester_grades_summary <- cleaned_data |>
     first_semester_dfw = mean(dfw, na.rm = TRUE)
   )
 
-first_two_semesters <- cleaned_data |> 
+first_two_semesters <- extracting_sem_data |> 
   group_by(stc_person) |> 
   arrange(semester_index) |> 
   filter(row_number() <= 2) |> 
