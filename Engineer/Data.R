f8d63e62a8cc520febe66195823ced206f3764c5
tarihart20 <tari.hart@yotes.collegeofidaho.edu>
2026-01-22 13:21:52 -0700

    data shows all rows along with double majors split

--- Engineer/Data.R
+++ Engineer/Data.R
@@ -4,9 +4,10 @@ library(dplyr)
 registrar_data <- read_csv('data/raw/registrar_data.csv')
 
 clean_data <- registrar_data |>
+  separate_longer_delim(stc_depts, delim = ',') |>
+  mutate(stc_depts = trimws(stc_depts)) |>
   group_by(stc_person, term_reporting_year) |>
-  mutate(major = first(stc_depts)) |>
-  ungroup()
+  mutate(major = first(stc_depts))
 
 glimpse(clean_data)
 
