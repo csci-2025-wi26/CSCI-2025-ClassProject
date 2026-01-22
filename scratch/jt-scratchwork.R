68162cf303109ec5bc359970924f00e2be1968ef
jackptheiss <jack.theiss@yotes.collegeofidaho.edu>
2026-01-22 01:00:32 -0700

    pushing changes to scratchwork

--- scratch/jt-scratchwork.R
+++ scratch/jt-scratchwork.R
@@ -6,6 +6,7 @@ glimpse(cleaned_data)
 
 
 
+
 #column E looks problematic: commas in double majors. if someone is selecting majors we want 
 #... double major "John" to be detected by major A or major B
 
@@ -60,10 +61,62 @@ cleaned_data |>
   # filter(is.na(division)) |> 
   glimpse()
 
-
- # mutate(crs_no_numeric = as.numeric(crs_no)) |> 
+cleaned_data |> 
+  mutate(crs_no_numeric = as.numeric(crs_no)) |> 
 
 
 #there are no NA course numbers. it's the XX's and "L's" that are causing me problems above.
 cleaned_data |> 
   count(crs_no) |> arrange(n)
+
+
+
+
+
+
+
+cleaned_data |> 
+  select(stc_course_name, 
+    stc_sec_name, 
+    student_course_sec_stc_title, 
+    crs_no) |> 
+  filter(str_detect(crs_no, "^/d{4}$")) |> 
+  glimpse()
+
+
+cleaned_data |> 
+  mutate(crs_level = case_when(str_detect(crs_no, "^1.*") ~ "1XX",
+                              str_detect(crs_no, "^2.*") ~ "2XX",
+                              str_detect(crs_no, "^3.*")~ "3XX", 
+                              str_detect(crs_no, "^4.*") ~ "4XX", 
+                              str_detect(crs_no, "^5.*") ~ "5XX", 
+                              str_detect(crs_no, "^6.*") ~ "6XX"
+                            )
+                          ) |> 
+  filter(!str_detect(crs_level, "^\\dXX$")) |>     #test - returns nothing, so nothing left out.
+  glimpse()
+
+
+
+
+glimpse(cleaned_data)
+
+cleaned_data |> 
+  count(students_stu_class)
+
+cleaned_data |> 
+  mutate(students_stu_class = str_remove_all(students_stu_class, ",")) |> 
+  count(students_stu_class) |> 
+  # filter(is.na(students_stu_class)) |> 
+  # filter(stc_acad_level != "GR") |> 
+  glimpse()
+
+#^confirms all the na's in students_stu_class are from the Graduate students
+
+cleaned_data |> 
+  select(stc_acad_level, students_stu_class) |> 
+  filter(stc_acad_level == "GR") |> 
+  glimpse()
+
+cleaned_data |> 
+  count(stc_acad_level)
