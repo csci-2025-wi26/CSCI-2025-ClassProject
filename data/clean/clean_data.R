e4ac5ee6ac0606d1f60016bcb9fa38412f845953
Asher Bruce <bruce.asher.j@gmail.com>
2026-01-22 18:41:03 -0700

    identify issue in coding between grad_app and stu_majors, add more options to gender, create major_switched columns (need names to be same between stu_majors and xstu_grad_app_major)

--- data/clean/clean_data.R
+++ data/clean/clean_data.R
@@ -4,7 +4,6 @@ library(janitor)
 raw_data <- read.csv("data/raw/registrar_data.csv") |>
   clean_names()
 
-glimpse(raw_data)
 excluded_cols <- c(
   "person_xper_chosen_last_name",
   "person_xper_chosen_nick_first_name",
@@ -18,6 +17,7 @@ excluded_cols <- c(
   "minors_minors_desc",
   "students_stu_active_minors"
 )
+
 cleaned_data <- raw_data |>
   mutate(across(where(is.character), ~ na_if(.x, "NA") |> na_if("NONE"))) |>
   mutate(
@@ -43,7 +43,31 @@ cleaned_data <- raw_data |>
   mutate(
     primary_major = coalesce(students_stu_active_majors, stu_acad_programs)
   ) |>
+  mutate( # students_xstu_grad_app_major align with coding in students_stu_active_majors and students_stu_majors
+    students_xstu_grad_app_major = if_else(
+      students_xstu_grad_app_major == "BUSMDM", 
+      "BUMDM", 
+      students_xstu_grad_app_major
+    ),
+    students_xstu_grad_app_major = if_else(
+      str_detect(students_xstu_grad_app_major, "\\."),
+      str_remove_all(students_xstu_grad_app_major, "\\.B\\w"),
+      students_xstu_grad_app_major
+    ),
+    students_xstu_grad_app_major = if_else(
+      str_detect(students_xstu_grad_app_major, "�"),
+      str_replace_all(students_xstu_grad_app_major, "�", ","),
+      students_xstu_grad_app_major
+    ),
+    across(
+      c(students_xstu_grad_app_major, students_stu_majors),
+      ~ if_else(. == "ACCT", "ACC", .)
+    )
+  ) |> 
   select(-all_of(excluded_cols))
+
+# *** LOOK AT THIS ***
+setdiff(cleaned_data$students_xstu_grad_app_major, cleaned_data$students_stu_majors)
 
 # add a refined grad column
 cleaned_data <- cleaned_data |>
@@ -70,8 +94,12 @@ cleaned_data <- cleaned_data |>
 
 cleaned_data <- cleaned_data |>
   mutate(
-    refined_gender = case_when(
+    gender = case_when( # when visualizing, male/female and not male/female
       person_gender_identity == "TRANSGEN" ~ "Transgender",
+      person_gender_identity == "AGEND" ~ "Agender",
+      person_gender_identity == "BIGEND" ~ "Bigender",
+      person_gender_identity == "POLYGEND" ~ "Polygender",
+      person_gender_identity == "GENFLU" ~ "Genderfluid",
       TRUE ~ gender
     )
   ) |>
@@ -100,16 +128,44 @@ cleaned_data <- cleaned_data |>
   mutate(classes_taken = n())
 
 
-cleaned_data <- cleaned_data |> #col to look at major switching
-  group_by(stc_person) |>
+major_switched <- cleaned_data |> #col to look at major switching
+  select(
+    stc_person,
+    students_stu_majors,
+    students_xstu_grad_app_major
+  ) |> 
+  distinct(stc_person, .keep_all = TRUE) |>
+  mutate(
+    majors = if_else( # remove NON and OPEN majors
+      str_detect(students_stu_majors, ","), 
+      str_remove_all(students_stu_majors, ",?NON,?|,?OPEN,?"), 
+      NA
+    ),
+    dropped_majors_list = map2_chr( # list of dropped majors, separated by ", "
+      majors,
+      students_xstu_grad_app_major,
+      ~ {
+        if (is.na(.x) || is.na(.y)) {
+          return(NA_character_)
+        }
 
-  ungroup()
+        applied <- str_split(.x, ",", simplify = TRUE)
+        current <- str_split(.y, ",", simplify = TRUE)
+        dropped <- setdiff(applied, current)
 
-glimpse(cleaned_data)
+        if_else(setequal(applied, current), NA, str_flatten_comma(dropped))
+      }
+    ),
+    switched_majors = if_else(is.na(dropped_majors_list), FALSE, TRUE), # lgl, did switch majors?
+    # .keep = "unused"
+  ) |> 
+  arrange(dropped_majors_list) |> 
+  View()
+
+cleaned_data <- cleaned_data |> 
+  left_join(major_switched, join_by(stc_person))
 
 write_csv(
   cleaned_data,
   "data/clean/registrar_cleaned.csv"
 )
-
-glimpse(cleaned_data)
