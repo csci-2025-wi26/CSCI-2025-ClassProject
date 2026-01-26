29f8996f22e15062630d1283617b1340d44b8a8b
tarihart20 <tari.hart@yotes.collegeofidaho.edu>
2026-01-25 21:17:35 -0700

    file

--- Engineer/Data.R
+++ Engineer/Data.R
@@ -4,9 +4,9 @@ library(dplyr)
 enrollment_registrar_data <- read_csv('data/raw/registrar_data.csv')
 
 enrollment_clean_data <- enrollment_registrar_data |>
-  separate_longer_delim(students_stu_majors , delim = ',') |>
+  separate_longer_delim(students_stu_majors, delim = ',') |>
   separate_longer_delim(person_per_races, delim = ',') |>
-  mutate(students_stu_majors  = trimws(students_stu_majors )) |>
+  mutate(students_stu_majors  = trimws(students_stu_majors)) |>
   group_by(stc_person, term_reporting_year) |>
   mutate(major = first(students_stu_majors))
 
