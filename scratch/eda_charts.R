447a2a9cf8e6cc704246879f78b6f371eced379c
ChaseForeman <foremanchas0@gmail.com>
2026-01-25 17:46:31 -0700

    Make new charts (not yet standardized)

--- scratch/eda_charts.R
+++ scratch/eda_charts.R
@@ -1,42 +1,7 @@
 library(tidyverse)
 library(dplyr)
 
-raw_data <- read_csv('data/raw/registrar_data.csv')
-
-#adding dfw column
-clean_data <- raw_data |> 
-  mutate(dfw = case_when(
-    xstc_verified_lettr_grade %in% c("A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "P") ~ 1, 
-    xstc_verified_lettr_grade %in% c('W', "F", "D-", "D", "D+")  ~ 0
-  ))
-
-#__________add numeric gpa_______________
-
-updated_clean_data <- clean_data |> 
-  mutate(grade_numeric = case_when(
-    xstc_verified_lettr_grade == "A" ~ 4.0,
-    xstc_verified_lettr_grade == "A-" ~ 3.70,
-    xstc_verified_lettr_grade == "B+" ~ 3.30,
-    xstc_verified_lettr_grade == "B" ~ 3.00,
-    xstc_verified_lettr_grade == "B-" ~ 2.70,
-    xstc_verified_lettr_grade == "C+" ~ 2.30,
-    xstc_verified_lettr_grade == "C" ~ 2.00,
-    xstc_verified_lettr_grade == "C-" ~1.70,
-    xstc_verified_lettr_grade == "D+" ~ 1.30,
-    xstc_verified_lettr_grade == "D" ~ 1.00,
-    xstc_verified_lettr_grade == "D-" ~ 0.70,
-    xstc_verified_lettr_grade == "F" ~ 0.00 
-  )) 
-
-gradeAverageDepartment <- updated_clean_data |> 
-  select(stc_person, xstc_verified_lettr_grade, dfw, grade_numeric, stc_depts) |> 
-  group_by(stc_depts) |> 
-  mutate(mean_department = mean(grade_numeric, na.rm = TRUE)) |>
-  mutate(stc_depts = fct_inorder(stc_depts)) |>
-  arrange(mean_department) |>
-  distinct(stc_depts, mean_department)
-
-print(gradeAverageDepartment, n = 26)
+cleaned_data <- read_csv("data/processed/cleaned_data.csv")
 
 mean_scores <- gradeAverageDepartment |>
   ggplot(aes(x = reorder(stc_depts, mean_department), y = mean_department, fill = mean_department >= 3.30)) +
@@ -57,9 +22,62 @@ mean_scores <- gradeAverageDepartment |>
   labs(fill = "Grade",
     x = "Department",
     y = "Mean Score",
-    title = "Mean Grade Score by Department",
+    title = "Mean Grade Score by Department"
     )
 
 mean_scores
 
 ggsave("plots/mean_scores.png", plot = mean_scores, width = 12, height = 6)
+
+#____________________
+
+dfwStanding <- cleaned_data |>
+  filter(!is.na(students_stu_class)) |>
+  ggplot(aes(x = students_stu_class, y = dfw, na.rm = TRUE)) +
+  geom_col(fill = "#533860") +
+  labs(
+    x = "Standing",
+    y = "Students with a D, F, or W",
+    title = "DFW by Standing"
+  ) +
+  theme_minimal() +
+  theme(
+    title = element_text(size = 18, family = "Proxima Nova"),
+    axis.text.x = element_text(size = 12),
+  )
+
+ggsave("plots/dfw_by_standing.png", plot = dfwStanding, width = 12, height = 6)
+
+#________________
+
+cleaned_data$class_level <- str_extract(cleaned_data$stc_course_name, "(?<=-)\\d") |> as.factor()
+
+dfwDeptMath <- cleaned_data |>
+  filter(!is.na(students_stu_class),
+          stc_depts %in% "MATPH") |>
+  ggplot(aes(x = interaction(class_level, stc_course_name, lex.order = TRUE), y = dfw, na.rm = TRUE, fill = class_level)) +
+  geom_col() +
+  scale_fill_manual(
+    values = c(
+    "1" = "#533860",
+    "2" = "#228B22",
+    "3" = "#00BFFF",
+    "4" = "#E2725B"
+  )) +
+  scale_x_discrete(labels = cleaned_data$stc_course_name) +
+  labs(
+    x = "Classes (Math Department)",
+    y = "Students with a D, F, or W",
+    title = "DFW by Class (Math Department)"
+  ) +
+  theme_minimal() +
+  theme(
+    title = element_text(size = 18, family = "Proxima Nova"),
+    axis.text.x = element_text(size = 12, )
+    
+  ) +
+  coord_flip()
+
+ggsave("plots/dfw_by_dept_math.png", plot = dfwDeptMath, width = 10, height = 16)
+
+
